84107c004de6431db161629a66aa49fc93586562
Zamarise Montoya <zamarise@gmail.com>
2018-04-05 20:55:45 -0700

    Add intital code to server and index

--- server/index.js
+++ server/index.js
@@ -1,1 +1,6 @@
-const express = require('express');
+/* eslint no-console: "off" */
+const server = require('./server');
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
