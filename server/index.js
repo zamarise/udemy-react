9a497a1fc5b579a71f062396894a3eec2bff668a
Zamarise Montoya <zamarise@gmail.com>
2018-04-09 19:17:28 -0700

    Update server port number

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 /* eslint no-console: "off" */
 const server = require('./server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
